bd654ce903fa3d8def8c7f2ad2ea94821d65efe0
dat <you@example.com>
2021-09-03 19:21:13 +0700

    show cart page, ps: no css

--- src/features/Cart/index.js
+++ src/features/Cart/index.js
@@ -1,3 +1,4 @@
+import { Box } from '@material-ui/core';
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { cartTotalSelector } from './selectors';
@@ -6,8 +7,22 @@ CartFeature.propTypes = {};
 
 function CartFeature(props) {
   const cartTotal = useSelector(cartTotalSelector);
+  const cartItems = useSelector((state) => state.cart.cartItems);
 
-  return <div>Cart{cartTotal}</div>;
+  const element = cartItems.map((cartItem) => (
+    <ul key={cartItem.id}>
+      <li>
+        {cartItem.product.name} {cartItem.product.salePrice} {cartItem.quantity}
+      </li>
+    </ul>
+  ));
+
+  return (
+    <Box>
+      {element}
+      Total {cartTotal}
+    </Box>
+  );
 }
 
 export default CartFeature;
